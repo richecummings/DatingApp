e9617234d3fcee7bb28e32b535e9d4b035c9108a
Richard Cummings <richardecummings@gmail.com>
2019-02-27 11:02:48 -0500

    Adding AutoMapper

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -1,35 +1,44 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp.API.Data.Interfaces;
+using DatingApp.API.Dtos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DatingApp.API.Controllers 
+namespace DatingApp.API.Controllers
 {
     [Authorize]
     [Route ("api/[controller]")]
     [ApiController]
-    public class UsersController : ControllerBase 
+    public class UsersController : ControllerBase
     {
         private readonly IDatingRepository _repository;
-        public UsersController (IDatingRepository repository) 
+        private readonly IMapper _mapper;
+        public UsersController (IDatingRepository repository, IMapper mapper)
         {
+            _mapper = mapper;
             _repository = repository;
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetUsers() 
+        public async Task<IActionResult> GetUsers ()
         {
-            var users = await _repository.GetUsers();
+            var users = await _repository.GetUsers ();
 
-            return Ok(users);
+            var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
+
+            return Ok (usersToReturn);
         }
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetUser(int id)
+        [HttpGet ("{id}")]
+        public async Task<IActionResult> GetUser (int id)
         {
-            var user = await _repository.GetUser(id);
+            var user = await _repository.GetUser (id);
 
-            return Ok(user);
+            var userToReturn = _mapper.Map<UserForDetailedDto>(user);
+
+            return Ok (userToReturn);
         }
     }
 }
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp.API.Data;
 using DatingApp.API.Data.Interfaces;
 using DatingApp.API.Data.Repositories;
@@ -42,6 +43,7 @@ namespace DatingApp.API
                     opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
                 });
             services.AddCors();
+            services.AddAutoMapper();
             services.AddTransient<Seed>();
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddScoped<IDatingRepository, DatingRepository>();
