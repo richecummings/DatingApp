964a96402ebd534a88624059f5643f4f6198fc6b
Richard Cummings <richardecummings@gmail.com>
2019-03-03 15:13:43 -0500

    Adding the set main photo functionality to the SPA

--- DatingApp.API/Data/Repositories/DatingRepository.cs
+++ DatingApp.API/Data/Repositories/DatingRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data.Interfaces;
 using DatingApp.API.Models;
@@ -47,6 +48,11 @@ namespace DatingApp.API.Data.Repositories
             var photo = await _context.Photos.FirstOrDefaultAsync(p => p.Id == id);
 
             return photo;
+        }
+
+        public async Task<Photo> GetMainPhotoForUser(int userId)
+        {
+            return await _context.Photos.Where(u => u.UserId == userId).FirstOrDefaultAsync(p => p.IsMain);
         }
     }
 }
