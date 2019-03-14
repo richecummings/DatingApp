5415728bf2be1d04a0ac1e92ad4f3c996866942f
Richard Cummings <richardecummings@gmail.com>
2019-03-14 19:01:38 -0400

    Returning the users with their roles

--- DatingApp.API/Controllers/AdminController.cs
+++ DatingApp.API/Controllers/AdminController.cs
@@ -1,5 +1,9 @@
+using System.Threading.Tasks;
+using System.Linq;
+using DatingApp.API.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
@@ -7,11 +11,30 @@ namespace DatingApp.API.Controllers
     [Route("api/[controller]")]
     public class AdminController : ControllerBase
     {
+        private readonly DataContext _context;
+        public AdminController(DataContext context)
+        {
+            _context = context;
+        }
+
         [Authorize(Policy = "RequireAdminRole")]
         [HttpGet("usersWithRoles")]
-        public IActionResult GetUsersWithRoles()
+        public async Task<IActionResult> GetUsersWithRoles()
         {
-            return Ok("Only admins can see this");
+            var userList = await (
+                from user in _context.Users orderby user.UserName
+                select new
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Roles = (from userRole in user.UserRoles
+                                join role in _context.Roles
+                                on userRole.RoleId
+                                equals role.Id
+                                select role.Name).ToList()
+                }).ToListAsync();
+
+            return Ok(userList);
         }
 
         [Authorize(Policy = "ModeratePhotoRole")]
