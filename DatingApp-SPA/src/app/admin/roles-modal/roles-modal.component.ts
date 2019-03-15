f6740d223ceefeeb135552477378d64d839777bf
Richard Cummings <richardecummings@gmail.com>
2019-03-15 18:54:42 -0400

    Passing the roles data into the modal

--- DatingApp-SPA/src/app/admin/roles-modal/roles-modal.component.ts
+++ DatingApp-SPA/src/app/admin/roles-modal/roles-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap';
+import { User } from 'src/app/_models/user';
 
 @Component({
   selector: 'app-roles-modal',
@@ -7,9 +8,8 @@ import { BsModalRef } from 'ngx-bootstrap';
   styleUrls: ['./roles-modal.component.css']
 })
 export class RolesModalComponent implements OnInit {
-  title: string;
-  closeBtnName: string;
-  list: any[] = [];
+  user: User;
+  roles: any[];
 
   constructor(public bsModalRef: BsModalRef) {}
 
