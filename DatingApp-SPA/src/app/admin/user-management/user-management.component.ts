97a7e9c72ab3be24eaccc2a0bd3770b58125db6b
Richard Cummings <richardecummings@gmail.com>
2019-03-15 19:15:36 -0400

    Passing data out of the modal and hooking up into the API

--- DatingApp-SPA/src/app/admin/user-management/user-management.component.ts
+++ DatingApp-SPA/src/app/admin/user-management/user-management.component.ts
@@ -36,7 +36,19 @@ export class UserManagementComponent implements OnInit {
       roles: this.getRolesArray(user)
     };
     this.bsModalRef = this.modalService.show(RolesModalComponent, {initialState});
-    this.bsModalRef.content.closeBtnName = 'Close';
+    this.bsModalRef.content.updateSelectedRoles.subscribe((values) => {
+      const rolesToUpdate = {
+        roleNames: [...values.filter(el => el.checked === true).map(el => el.name)]
+      };
+      if (rolesToUpdate) {
+        this.adminService.updateUserRoles(user, rolesToUpdate).subscribe(() => {
+          user.roles = [...rolesToUpdate.roleNames];
+        }, error => {
+          console.log(error);
+          alertify.error(error);
+        });
+      }
+    });
   }
 
   private getRolesArray(user) {
